fb8f5156161234abbf7ac8d41bdae433603ba101
20161104604 <a20161104604@CIEC-iMacs-iMac.local>
2018-06-27 11:38:37 +0800

    Xiugai4

--- main.cpp
+++ main.cpp
@@ -6,34 +6,4 @@
 //  Copyright © 2018年 20161104604. All rights reserved.
 //
 
-
 #include <iostream>
-#include <cstring>
-#include <String>
-using namespace std;
-
-struct Student
-{
-    int no;
-    string name;
-    string sex;
-    string program;
-    string form;
-    string Class;
-    string tel;
-    int f;
-    double score[10];
-    double min;
-    double max;
-    double ave;
-    
-};
-struct Referee
-{
-    string name;
-    string sex;
-    string tel;
-};
-// 函数声明
-void  output ( student *s,int n,referee *r);
-void search(student *s,referee *r);
