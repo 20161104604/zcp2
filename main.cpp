7cee1afec1812c63952538fd42bac36cb9434bfc
20161104604 <a20161104604@CIEC-iMacs-iMac.local>
2018-06-27 11:17:09 +0800

    Xiugai1

---
+++ main.cpp
@@ -0,0 +1,38 @@
+//
+//  main.swift
+//  zcp1
+//
+//  Created by 20161104604 on 2018/6/27.
+//  Copyright © 2018年 20161104604. All rights reserved.
+//
+
+
+#include <iostream>
+#include <cstring>
+#include <String>
+using namespace std;
+
+struct Student
+{
+    int no;
+    string name;
+    string sex;
+    string program;
+    string form;
+    string Class;
+    string tel;
+    int f;
+    double score[10];
+    double min;
+    double max;
+    double ave;
+    
+};
+struct Referee
+{
+    string name;
+    string sex;
+    string tel;
+};
+// 函数声明
+void  output (
